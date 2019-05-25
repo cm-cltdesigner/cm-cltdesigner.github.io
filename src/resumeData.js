a0c3a42d07ecbd6e58b8b2d8e71c3399edf8931b
courtney manery <courtneymmanery@gmail.com>
2019-05-25 01:49:38 -0400

    FIRST COMMIT

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,63 +1,69 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "imagebaseurl":"http://www.courtneymanery.com",
+    "name": "Courtney Manery",
+    "role": "Graphic/ Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/courtneymanery",
+    "roleDescription":" I am someone who strives for results yet enjoys the creative design process to get there efficiently. I am a very client-focused, reliable and detail-oriented developer who enjoys learning new tech.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/courtneymanery",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/cm-cltdesigner",
           "className":"fa fa-github"
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
+          "name":"facebook",
+          "url":"https://www.facebook.com/COURTNEYMANERYDESIGNS/",
+          "className":"fa fa-facebook"
+        },
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme":"Graphic Design is my first love with an ceaseless passion for Front-End Web Development. I have extensive knowledge of modern Web techniques with random cravings for Hot Chocolate. Currently seeking a full-time opportunity with a talented and passionate group of developers/designers creating something awesome everyday. I believe in great leadership and teamwork, as well as being involved in an organization that believes in gaining a competitive edge and giving back to the community.",
+    "address":"Charlotte, NC USA",
+    "website":"courtneymanery.com",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"University of North Carolina",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019",
+        "Achievements":"Certification"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
+        "UniversityName":"Southern New Hampshire",
+        "specialization":"Marketing",
         "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
+        "YearOfPassing":"2019",
+        "Achievements":"Bachelors"
+      },
+      {
+        "UniversityName":"King's College",
+        "specialization":"Graphic Design",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2017",
+        "Achievements":"Associates"
+      },
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"GitCreativ LLC",
+        "specialization":"Graphic/ Web Developer",
+        "MonthOfLeaving":"2016",
+        "YearOfLeaving":"- Present",
+        "Achievements":"Developing engaging and innovative Digital and Print Designs, Logo and Web Design, Company Branding Development, Social Media Management"
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Allen Tate Companies",
+        "specialization":"Marketing Concierge",
+        "MonthOfLeaving":"2018",
+        "YearOfLeaving":"- 2019",
+        "Achievements":"Social Media Management, Create Custom Digital Ads & Direct Mailers, Host Monthly Workshops To Enhance Real Estate Agents Marketing/Advertising, Develop Luxury Portfolio Properties Presentations For Real Estate Agents"
       }
     ],
-    "skillsDescription":"Your skills here",
+    "skillsDescription":"Languages",
     "skills":[
       {
         "skillname":"HTML5"
@@ -66,19 +72,25 @@ let resumeData = {
         "skillname":"CSS"
       },
       {
-        "skillname":"Reactjs"
-      }
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"React.js"
+      },
+      {
+        "skillname":"Adobe Creative Suite"
+      },
     ],
     "portfolio":[
       {
-        "name":"project1",
+        "name":"MovieNow App",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "imgurl":"https://i.ibb.co/ChWJ3px/modified-logo.png"
       },
       {
-        "name":"project2",
+        "name":"Friend Finder App",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "imgurl":"https://i.ibb.co/WWvtqWp/fff-app.jpg"
       },
       {
         "name":"project3",
@@ -89,17 +101,33 @@ let resumeData = {
         "name":"project4",
         "description":"mobileapp",
         "imgurl":"images/portfolio/phone.jpg"
-      }
+      },
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Outside of being a great designer, Courtney is a great person. She puts the people around her in a better mood and always goes beyond what is asked of her without hesitation. She is a highly conceptual thinker and puts together highly developed, thorough pieces. She would be a great fit on any creative team.",
+        "name":"Kenneth D."
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
+        "description":"Beyond being a very motivated and talented graphic design student, Courtney served as a role model for other students. She's polite, friendly and a pleasure to have in any professional setting.",
+        "name":"Ralph V."
+      },
+      {
+        "description":"Courtney Manery did an outstanding job on a marketing brochure for my business. She was creative and attentive to my needs regarding the overall objective for the piece. Any client can count on Courtney to provide fast service and strong desire to get the job done right!",
+        "name":"Teresa R."
+      },
+      {
+        "description":"Courtney made sure she gave me exactly what I wanted. She understood my vision and gave me a logo that reflects me.",
+        "name":"Jessica M."
+      },
+      {
+        "description":"She did an amazing job with my company website. She added design details that I wouldn't never thought of which generated business. You too should hire Courtney.",
+        "name":"Shannon M."
+      },
+      {
+        "description":"The work mrs.manery done for my company was amazing she made my vision come true with a little twist will hire her for my next project thanks...",
+        "name":"Dontè M."
+      },
     ]
   }
   
